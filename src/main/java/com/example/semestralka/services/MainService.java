034336a4096639987b9b56c542d570126918789d
Jaroslav Vondrák <jaroslavvondrak12@seznam.cz>
2021-05-12 18:40:17 +0200

    delete

--- src/main/java/com/example/semestralka/services/MainService.java
+++ src/main/java/com/example/semestralka/services/MainService.java
@@ -24,30 +24,6 @@ public class MainService {
 
     @PostConstruct
     private void start(){
-        log.info("DEBUG");
 
-        /*Department d = new Department("technika", "IT");
-        departmentRepository.save(d);
-        d = new Department("technika2","IT2");
-        departmentRepository.save(d);
-        d = new Department("technika3","IT3");
-        departmentRepository.save(d);
-
-        Specialization s = new Specialization("special1", "SPEC1");
-        specializationRepository.save(s);
-        s = new Specialization("special2","SPEC2");
-        specializationRepository.save(s);
-        s = new Specialization("special3","SPEC3");
-        specializationRepository.save(s);
-
-        Date f = Date.valueOf("1991-03-31");
-        Employees a = new Employees("Jaroslav2", "Vondrak", f, d, s);
-        employeesRepository.save(a);*/
-
-        //Optional<Department> d = departmentRepository.findById((long) 1);
-        //Optional<Employees> e = employeesRepository.findById((long) 1);
-
-        //Employees e = new Employees("Jaroslav2", d.get());
-        //employeesRepository.save(e);
     }
 }
